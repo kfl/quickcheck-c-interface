fabe2f34c32842fe30a4dcb6ccaf2cee547de177
Ken Friis Larsen <ken@friislarsen.net>
2015-10-22 21:26:27 +0200

    OK, this time I got it right. Damn, binary search.
    Also added a main function to bsearch.c file, to allow some local testing.

--- src/bsearch.c
+++ src/bsearch.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int binsearch1(int *arr, size_t len, int key) {
   size_t l=0, u=len;
@@ -25,4 +26,31 @@ int binsearch2(int *arr, size_t len, int key) {
   }
   
   return -1;
+}
+
+int binsearch3(int *arr, size_t len, int key) {
+  if(len > 0) {
+    size_t l=0, u=len-1;
+
+    while(l <= u) {
+      size_t m = (size_t)((l+u)/2);
+      if      (arr[m] < key) l = m+1;
+      else if (arr[m] > key) u = m-1;
+      else return m;
+    }
+  }
+  
+  return -1;
+}
+
+
+
+int main() {
+  int arr[5] = {10,20,30,40,50};
+  printf("Res: %d\n", binsearch3(arr, 5, 10));
+  printf("Res: %d\n", binsearch3(arr, 5, 50));
+  printf("Res: %d\n", binsearch3(arr, 5, 40));
+  printf("Res: %d\n", binsearch3(arr, 5, 100));
+  printf("Res: %d\n", binsearch3(arr, 0, 10));
+  return 0;
 }
